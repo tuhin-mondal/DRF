772da567e9184bf8d66d7359d5d83dbed08ff61f
Tuhin Mondal <mtuhin255@gmail.com>
2023-11-16 20:12:50 +0530

    GET POST

--- drinks/views.py
+++ drinks/views.py
@@ -3,10 +3,18 @@ from rest_framework.decorators import api_view
 from .models import Drink
 from .serializers import DrinkSerializers
 from rest_framework.response import Response
+from rest_framework import status
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def drink_list(request):
 
-    drinks = Drink.objects.all()
-    serializer = DrinkSerializers(drinks,many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':    
+       drinks = Drink.objects.all()
+       serializer = DrinkSerializers(drinks,many=True)
+       return Response(serializer.data)
+    
+    if request.method == 'POST':
+       serializer = DrinkSerializers(data=request.data)
+       if serializer.is_valid():
+          serializer.save()
+          return Response(serializer.data,status=status.HTTP_201_CREATED)
