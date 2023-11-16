33acd64ab594532425bb5ffa4aa7684bb1241248
Tuhin Mondal <mtuhin255@gmail.com>
2023-11-16 19:53:55 +0530

    put req

--- drinks/views.py
+++ drinks/views.py
@@ -1,12 +1,40 @@
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from rest_framework.decorators import api_view
 from .models import Drink
 from .serializers import DrinkSerializers
 from rest_framework.response import Response
+from rest_framework import status
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def drink_list(request):
 
-    drinks = Drink.objects.all()
-    serializer = DrinkSerializers(drinks,many=True)
-    return Response(serializer.data)
+    if request.method == 'GET': 
+       drinks = Drink.objects.all()
+       serializer = DrinkSerializers(drinks,many=True)
+       return Response(serializer.data)
+    
+    if request.method == 'POST':
+        serializer = DrinkSerializers(data=(request.data))
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+
+@api_view(['GET','PUT','DELETE'])
+def drink_detail(request,id):
+
+    drinks = Drink.objects.get(pk=id)
+
+    if request.method == 'GET':
+        seriazer = DrinkSerializers(drinks)
+        return Response(seriazer.data)  
+
+    elif request.method == 'PUT':
+        seriazer = DrinkSerializers(drinks,data=request.data)
+        if seriazer.is_valid():
+            seriazer.save()
+            return Response(seriazer.data)
+
+    elif request.method == 'DELETE':
+        drinks.delete()
+        return Response(status=status.HTTP_400_BAD_REQUEST)
