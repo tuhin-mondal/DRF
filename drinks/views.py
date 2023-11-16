da617670b37bfe0ad99082fd24dde1f904be9438
Tuhin Mondal <mtuhin255@gmail.com>
2023-11-16 20:39:07 +0530

    GET,PUT,DELETE,POST

--- drinks/views.py
+++ drinks/views.py
@@ -18,3 +18,27 @@ def drink_list(request):
        if serializer.is_valid():
           serializer.save()
           return Response(serializer.data,status=status.HTTP_201_CREATED)
+       
+
+@api_view(['GET','POST','PUT','DELETE'])
+def drink_details(request,id): 
+   
+   drinks = Drink.objects.get(pk=id)
+
+   if request.method == 'GET':
+      serializers=DrinkSerializers(drinks)
+      return Response(serializers.data)
+   
+   elif request.method == 'POST':
+      pass    
+   
+   elif request.method == 'PUT':
+      serializers=DrinkSerializers(drinks,data=request.data)
+      if serializers.is_valid():
+         serializers.save()
+         return Response(serializers.data)
+      return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
+   
+   elif request.method == 'DELETE':
+      drinks.delete()
+      return Response(status=status.HTTP_204_NO_CONTENT)
